9e17ad92ee7a795018b0d872400d6571a20c2e98
Andres Parra <andeveling@gmail.com>
2024-08-24 13:02:16 -0500

    Add throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ProductsModule, UsersModule, AuthModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    AuthModule,
+    // Proteccion de ataques de brute force y ataques DDoS
+    ThrottlerModule.forRoot([
+      {
+        ttl: 180,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [],
   providers: [
     // {
