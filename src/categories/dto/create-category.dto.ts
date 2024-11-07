34ba8ba9aded303767defbf7ae0374b98c28cdb3
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2024-11-06 20:48:55 -0500

    Update requirements

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,10 +1,12 @@
-import { IsString, MaxLength, IsUUID } from 'class-validator';
+import { IsString, MaxLength, IsUUID, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsUUID()
   storeId: string;
 
   @IsString()
+  @MinLength(3)  
   @MaxLength(50)
+  @IsNotEmpty()  
   name: string;
 }
