f88c007e79468febfb04725011fe316b8a30f591
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2024-11-25 22:14:30 -0500

    Update columns

--- src/columns/dto/create-column.dto.ts
+++ src/columns/dto/create-column.dto.ts
@@ -1,5 +1,6 @@
 import { Board } from '@prisma/client';
-import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateColumnDto {
   @IsNotEmpty()
@@ -9,4 +10,9 @@ export class CreateColumnDto {
   @IsNotEmpty()
   @IsUUID()
   boardId: Board['id'];
+
+  @IsNotEmpty()
+  @IsInt()
+  @Type(() => Number)
+  position: number;
 }
